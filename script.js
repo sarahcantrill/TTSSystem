7123de34da5ebd5cf177faa237b0d2ae5565f07d
sarahcantrill <25324420@edgehill.ac.uk>
2025-03-04 22:03:01 +0000

    reset button functionality

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
     const wordboxes = document.querySelectorAll(".word-box");
     const textOutput = document.getElementById("text-output");
+    const resetButton = document.querySelector(".reset-btn");
 
     wordboxes.forEach(button => {
         button.addEventListener("click", function () {
            // addWord(this.text)
            textOutput.value += this.textContent + " ";
-        })
+        });
+    });
+
+    resetButton.addEventListener("click", () => {
+        textOutput.value = "";
     })
-})
+
+});
 
 
