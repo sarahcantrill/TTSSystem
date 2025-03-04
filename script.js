d7e189b99225ec58f7ca24f2925e503e1a44e9e0
sarahcantrill <25324420@edgehill.ac.uk>
2025-03-04 17:08:33 +0000

    adding word box functionality

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
     const wordboxes = document.querySelectorAll(".word-box");
-    const textOutput = getELementById("text-output");
+    const textOutput = document.getElementById("text-output");
+
+    wordboxes.forEach(button => {
+        button.addEventListener("click", function () {
+           // addWord(this.text)
+           textOutput.value += this.textContent + " ";
+        })
+    })
 })
 
 
