09fc8551697d14181fa651bd2572a68da047d5c4
sarahcantrill <25324420@edgehill.ac.uk>
2025-03-04 22:17:30 +0000

    undo button functionality

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ document.addEventListener("DOMContentLoaded", () => {
     const wordboxes = document.querySelectorAll(".word-box");
     const textOutput = document.getElementById("text-output");
     const resetButton = document.querySelector(".reset-btn");
+    const undoButton = document.querySelector(".undo-btn");
 
+    //words clicked means they are added to the container
     wordboxes.forEach(button => {
         button.addEventListener("click", function () {
            // addWord(this.text)
@@ -10,9 +12,18 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 
+    //reset button
     resetButton.addEventListener("click", () => {
         textOutput.value = "";
-    })
+    });
+
+    //undo button
+    undoButton.addEventListener("click", () => {
+        let wordLength = textOutput.value.trim().split(" ");
+        wordLength.pop(); //remove the last word 
+        textOutput.value = wordLength.join(" ") + " ";
+    });
+
 
 });
 
