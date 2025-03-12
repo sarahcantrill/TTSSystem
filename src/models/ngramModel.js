cfd1c52fb7a60cf731e96dd5a1e006ec5fb906b4
sarahcantrill <25324420@edgehill.ac.uk>
2025-03-12 16:36:44 +0000

    amending the ngram model for compromise

--- src/models/ngramModel.js
+++ src/models/ngramModel.js
@@ -1,11 +1,18 @@
 //import { NGram } from 'https://cdnjs.cloudflare.com/ajax/libs/ngram/2.0.0/ngram.min.js'; 
-import ngram from "https://cdn.skypack.dev/ngram";
+import nlp from "compromise";
 
 const corpus = "this is some ssample text for training"; //can be expanded for better predicitions
 
-const ngramModel =  newNGram (corpus, {n:2}); //creates new ngram model
+//const ngramModel =  newNGram (corpus, {n:2}); //creates new ngram model
 
 export function getSuggestions(input) {
-    const predictions = ngramModel.predict(input); //predicition based on input 
-    return predictions.slice(0, 10);
+    if (!input.trim()) return [];
+
+    const doc = nlp(corpus); //predicition based on input 
+    const words = doc
+        .terms()
+        .filter(term => term.text.toLowerCase().startsWith(input.toLowerCase()))
+        .out('array')
+
+        return words.slice(0, 10);
 }
