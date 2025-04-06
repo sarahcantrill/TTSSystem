6222cbacbb4a0c721b051065bac52d053efaee03
sarahcantrill <25324420@edgehill.ac.uk>
2025-04-06 17:14:17 +0100

    adding the topics word tab

--- prototype6/app.js
+++ prototype6/app.js
@@ -358,7 +358,8 @@ document.addEventListener("DOMContentLoaded", () => {
         'responses': ['yes', 'no', 'maybe', 'sort of', 'I agree', 'I disagree', 'I understand', 'I dont understand', 'I need help', 'good idea', 'can you explain more', 'Ill let you know'],
         'subjects': ['I', 'you', 'we', 'his', 'she', 'hers', 'they', 'them', 'their', 'it', 'the group'],
         'verbs': ['am', 'is', 'are', 'was', 'were', 'have', 'need', 'want', 'like', 'think', 'know', 'understand', 'finished'],
-        'common': ['and', 'or', 'but', 'because', 'with', 'without', 'to', 'from', 'in', 'on', 'at', 'by', 'for']
+        'common': ['and', 'or', 'but', 'because', 'with', 'without', 'to', 'from', 'in', 'on', 'at', 'by', 'for'],
+        'topics': ['idea', 'answer', 'question', 'presentation', 'topic', 'opinion', 'solution', 'point', 'example', 'group', 'project', 'task', 'sentence', 'word', 'slide', 'chart', 'diagram']
     };
 
     const improvedLanguageModel = {
