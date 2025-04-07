e50a1c75b4b87929bb9a1d34800718355fae32fc
sarahcantrill <25324420@edgehill.ac.uk>
2025-04-07 13:08:00 +0100

    increasing the questions tab content

--- prototype6/app.js
+++ prototype6/app.js
@@ -354,7 +354,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const categoryWords = {
         'starters': ['I', 'You', 'We', 'They', 'He', 'She', 'It', 'Please', 'Today', 'Could', 'Would'],
-        'questions': ['who', 'what', 'when', 'where', 'why', 'can I', 'could you', 'please', 'do you', 'Could', 'Would', 'is it', 'Can you help me?', 'Can you explain this?', 'What does this mean?', 'What does that mean?', 'Could you repeat that?', 'How do I do this?', 'How do I do that?', 'Is this correct?','Can you show me how to do this?', 'Can you show me how to do that?'],
+        'questions': ['who', 'what', 'when', 'where', 'why', 'can I', 'Can you help me?', 'Can you explain that?', 'What does this mean?', 'What does that mean?', 'Could you repeat that?', 'How do I do this?', 'How do I do that?', 'Is this correct?', 'Can you show me how to do this?', 'Can you show me how to do that?', 'Is this going to be on the exam?', 'Do we have any homework?', 'How are you?', 'What do you do?', 'What’s your name?', 'How old are you?', 'What time is it?', 'What do you think?', 'Are you alright?', 'What do you mean?', 'Can I ask you something?', 'What’s going on?', 'How many words does it have to be?', 'Can you check my work?', 'What does this word mean?'],
         'responses': ['yes', 'no', 'maybe', 'sort of', 'I agree', 'I disagree', 'I understand', 'I dont understand', 'I need help', 'good idea', 'can you explain more', 'Ill let you know'],
         'subjects': ['I', 'you', 'we', 'his', 'she', 'hers', 'they', 'them', 'their', 'it', 'the group'],
         'verbs': ['am', 'is', 'are', 'was', 'were', 'have', 'need', 'want', 'like', 'think', 'know', 'understand', 'finished'],
